3c85a94e1e3ca90b29bac88eb6f346c7a8355f47
Sheggybtw <pschenichniyvitaliy@gmail.com>
2023-04-18 20:40:05 +0300

    done app

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Button({text = 'theme', btnClass , icon, onClick}) {
+  return (
+    <button className={`btn ${btnClass}`} onClick={onClick}>{icon}{text}</button>
+  )
+}
---
+++ src/components/LightAndBlackMode.jsx
@@ -0,0 +1,37 @@
+/* eslint-disable default-case */
+
+import React, { useState, useEffect } from "react";
+import Blog from "./Blog";
+import { ThemeContext, themes } from "../context/theme-context";
+
+export default function LightAndDarkMode() {
+  const [theme, setTheme] = useState(themes.light);
+
+  function changeTheme() {
+    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light);
+  }
+
+  let docBody = document.body.classList;
+  useEffect(() => {
+    switch (theme) {
+      case themes.light:
+        docBody.remove("bg-dark");
+        docBody.remove("text-light");
+        docBody.add("bg-light");
+        docBody.add("text-dark");
+        break;
+      case themes.dark:
+        docBody.remove("bg-light");
+        docBody.remove("text-dark");
+        docBody.add("bg-dark");
+        docBody.add("text-light");
+        break;
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, changeTheme }}>
+      <Blog />
+    </ThemeContext.Provider>
+  );
+}
